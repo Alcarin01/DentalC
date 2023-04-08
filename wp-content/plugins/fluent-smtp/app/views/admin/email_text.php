d294e635dfbd5588cf13292d851d7518db78b40c
Abdel O <saidortiz31088@gmail.com>
2023-04-08 09:52:03 -0500

    kuyf

--- wp-content/plugins/fluent-smtp/app/views/admin/email_text.php
+++ wp-content/plugins/fluent-smtp/app/views/admin/email_text.php
@@ -2,4 +2,4 @@ Hi There,
 Are you seeing this email? You are? Well awesome - that means you're all set to start sending emails from your site.
 Thank you for using FluentSMTP 🎉
 
-This Email sent at (server time): 2023-04-08 14:30:05
+This Email sent at (server time): 2023-04-08 14:45:52
